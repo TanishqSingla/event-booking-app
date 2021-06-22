7c79ee2a87d17aec4c3963c06003cf614c80f324
TanishqSingla <tanishqsingla00@gmail.com>
2021-06-23 00:46:39 +0530

    Adding graphql and building schema

--- app.ts
+++ app.ts
@@ -1,13 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import graphqlHttp from 'express-graphql';
+import { buildSchema } from 'graphql'
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (_req, res, _next) => {
-  res.send('Hello World')
-})
+app.use('/graphql', graphqlHttp.graphqlHTTP({
+  schema: buildSchema(
+    `schema {
+      query: string
+      mutation: string
+    }`
+  ),
+  rootValue: {}
+}));
 
 app.listen(3000, () => {
   console.log("server is up and running at port 3000");
