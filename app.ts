5f763e285d4a7f3a1457c5a5ae88b34c749a5f11
TanishqSingla <tanishqsingla00@gmail.com>
2021-06-22 23:48:39 +0530

    testing route

--- app.ts
+++ app.ts
@@ -3,9 +3,11 @@ import bodyParser from "body-parser";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.json());
+
+app.get('/', (_req, res, _next) => {
+  res.send('Hello World')
+})
 
 app.listen(3000, () => {
   console.log("server is up and running at port 3000");
