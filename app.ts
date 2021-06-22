94428e7409150799461fcc2511cf139f074058f0
TanishqSingla <tanishqsingla00@gmail.com>
2021-06-22 23:31:51 +0530

    Adding tsconfig and development dependencies

--- app.ts
+++ app.ts
@@ -3,6 +3,10 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
 app.listen(3000, () => {
   console.log("server is up and running at port 3000");
 });
