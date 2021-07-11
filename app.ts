72a118eca197e2afdc77fd8efa2271272a922bbf
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-11 20:12:04 +0530

    Defining event

--- app.ts
+++ app.ts
@@ -3,19 +3,48 @@ import bodyParser from "body-parser";
 
 import * as graphqlHttp from 'express-graphql';
 import { buildSchema } from 'graphql'
+import { DESTRUCTION } from "dns";
 
 const app = express();
+
+interface Event {
+  title: string;
+  description: string;
+  price: number;
+  date: string
+}
+
+interface EventQuery {
+  eventInput: Event; 
+}
+
+const events: Array<Event> = [];
 
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp.graphqlHTTP({
   schema: buildSchema(
-    `type RootQuery {
-      events: [String!]!
+    `type Event {
+      _id: ID
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    type RootQuery {
+      events: [Event]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -25,11 +54,19 @@ app.use('/graphql', graphqlHttp.graphqlHTTP({
   ),
   rootValue: {
     events: () => {
-      return ['Opening note', 'Conference', 'Closing note']
+      return events
     },
-    createEvents: (args: any) => {
-      const eventName = args.name;
-      return eventName;
+    createEvents: (args: EventQuery) => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        }
+
+        events.push(event)
+        return event;
     },
   },
   graphiql: true
