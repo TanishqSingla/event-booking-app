a6bca5d75c1442d8b5c8a387d3f6bd7e1fc97b4b
TanishqSingla <tanishqsingla00@gmail.com>
2021-07-11 21:51:03 +0530

    Adding mongoose schema

--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-import * as graphqlHttp from 'express-graphql';
-import { buildSchema } from 'graphql';
-import * as mongoose from 'mongoose';
+import * as graphqlHttp from "express-graphql";
+import { buildSchema } from "graphql";
+import mongoose from "mongoose";
 import { PASSWORD, USERNAME } from "./secrets";
-
 
 const app = express();
 
@@ -13,20 +12,22 @@ interface Event {
   title: string;
   description: string;
   price: number;
-  date: string
+  date: string;
 }
 
 interface EventQuery {
-  eventInput: Event; 
+  eventInput: Event;
 }
 
 const events: Array<Event> = [];
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp.graphqlHTTP({
-  schema: buildSchema(
-    `type Event {
+app.use(
+  "/graphql",
+  graphqlHttp.graphqlHTTP({
+    schema: buildSchema(
+      `type Event {
       _id: ID
       title: String!
       description: String!
@@ -53,30 +54,35 @@ app.use('/graphql', graphqlHttp.graphqlHTTP({
       query: RootQuery
       mutation: RootMutation
     }`
-  ),
-  rootValue: {
-    events: () => {
-      return events
-    },
-    createEvents: (args: EventQuery) => {
+    ),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvents: (args: EventQuery) => {
         const event = {
           _id: Math.random().toString(),
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        }
+          date: args.eventInput.date,
+        };
 
-        events.push(event)
+        events.push(event);
         return event;
+      },
     },
-  },
-  graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
-mongoose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.aac6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(()=>
-  app.listen(3000, () => {
-  console.log("server is up and running at port 3000");
-})
-).catch(e => console.log(e))
-
+mongoose.connect(
+    `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.aac6e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then(() =>
+    app.listen(3000, () => {
+      console.log("server is up and running at port 3000");
+    })
+  )
+  .catch((e) => console.log(e));
